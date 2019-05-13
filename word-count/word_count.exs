3fdc5b36a34f480c9ced8355fdaf0c25e355b692
Edwin Allen <edwin.allen@gmail.com>
2019-05-12 20:08:46 -0600

    solve using Map.update

--- word-count/word_count.exs
+++ word-count/word_count.exs
@@ -6,16 +6,18 @@ defmodule Words do
   """
   @spec count(String.t()) :: map
   def count(sentence) do
-    sentence |> String.downcase |> String.split(~r{(*UTF)(_|[^\w-])+}) |> words_in_list(%{})
+    sentence |> String.downcase |> String.split(~r{(*UTF)(_|[^\w-])+}) |> words_in_list
   end
 
-  #recursive counting
+  # defp words_in_list(words) do
+  #   Enum.reduce(words, %{}, fn(word, map) ->
+  #     Map.update(map, word, 1, fn(current_value) -> current_value + 1 end)
+  #   end)
+  # end
+
   def words_in_list([], map), do: map
-  def words_in_list([head | tail], map) do
-    element_map  = case Map.has_key?(map, head) do
-                     true -> %{head => map[head] + 1 }
-                     false -> %{head => 1}
-                   end
-    words_in_list(tail, Map.merge(map, element_map))
+  def words_in_list([head | tail], map \\ %{}) do
+    updated_map = Map.update(map, head, 1, &(&1 + 1))
+    words_in_list(tail, updated_map)
   end
 end
