f668f46f7ecec3cf04e4039237c78420fa9ef67b
Edwin Allen <edwin.allen@gmail.com>
2019-05-14 11:24:58 -0600

    update word count with tream

--- word-count/word_count.exs
+++ word-count/word_count.exs
@@ -6,18 +6,18 @@ defmodule Words do
   """
   @spec count(String.t()) :: map
   def count(sentence) do
-    sentence |> String.downcase |> String.split(~r{(*UTF)(_|[^\w-])+}) |> words_in_list
+    sentence |> String.downcase |> String.split(~r{(*UTF)_|[^\w-]+}, trim: true) |> words_in_list
   end
 
-  # defp words_in_list(words) do
-  #   Enum.reduce(words, %{}, fn(word, map) ->
-  #     Map.update(map, word, 1, fn(current_value) -> current_value + 1 end)
-  #   end)
+  defp words_in_list(words) do
+    Enum.reduce(words, %{}, fn(word, map) ->
+      Map.update(map, word, 1, &(&1+1))
+    end)
+  end
+
+  # def words_in_list([], map), do: map
+  # def words_in_list([head | tail], map \\ %{}) do
+  #   updated_map = Map.update(map, head, 1, &(&1 + 1))
+  #   words_in_list(tail, updated_map)
   # end
-
-  def words_in_list([], map), do: map
-  def words_in_list([head | tail], map \\ %{}) do
-    updated_map = Map.update(map, head, 1, &(&1 + 1))
-    words_in_list(tail, updated_map)
-  end
 end
